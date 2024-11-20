368b346f1faad6fc58e62d6f34e5059bb0fc6b00
Adam Amanbaev <adam.amanbaev@gmail.com>
2024-11-20 22:48:58 +0100

    Add sol

--- cses/04_graph_algorithms/11_flight_discount.cpp
+++ cses/04_graph_algorithms/11_flight_discount.cpp
@@ -7,5 +7,39 @@ typedef long long ll;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    ;
+    ll n, m;
+    cin >> n >> m;
+
+    vector<vector<array<ll, 2>>> al(n);
+    for(ll i = 0; i < m; i++) {
+        ll a, b, c;
+        cin >> a >> b >> c, --a, --b;
+
+        al[a].push_back({b, c});
+    }
+
+    vector<array<ll, 2>> dist(n, {(ll)1e18, (ll)1e18});
+    priority_queue<array<ll, 3>, vector<array<ll, 3>>, greater<array<ll, 3>>> pq;
+    pq.push({0, 0, 0});
+    dist[0][0] = 0;
+    while(sz(pq)) {
+        auto [d, u, flag] = pq.top();
+        pq.pop();
+
+        if(d > dist[u][flag]) 
+            continue;
+
+        for(auto [v, w]: al[u]) {
+            if(dist[v][flag] > dist[u][flag] + w) {
+                dist[v][flag] = dist[u][flag] + w;
+                pq.push({dist[v][flag], v, flag});
+            }
+            if(!flag && dist[v][1] > dist[u][0] + w / 2) {
+                dist[v][1] = dist[u][0] + w / 2;
+                pq.push({dist[v][1], v, 1});
+            }
+        }
+    }
+
+    cout << dist[n - 1][1];
 }
